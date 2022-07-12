ca94fff6f471072cf79ace5dc4d534eabe9cb3ae
Taufik Alam <taufikalam759@gmail.com>
2022-07-12 23:29:39 +0530

    some library installed

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-// hello
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+app.get("/", (req, res) => {
+  res.send("Server running");
+});
+app.listen(port, () => {
+  console.log(`Server Running on port : ${"http://127.0.0.1:5000"}`);
+});
+
